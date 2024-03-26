0cdc22e5d1652fbf0b519535cee7cb4a63c6751a
Lee <you@example.com>
2024-03-26 23:37:44 +0300

    updated index.js

--- index.js
+++ index.js
@@ -21,3 +21,10 @@ myButton.onclick = function(){
     label3.textContent = randomNum3;
 
 }
+const email = "leekimutai254@gmail.com";
+
+let username = email.slice(0, email.indexOf("@"));
+let extension = email.slice(email.indexOf("@") + 1);
+
+console.log(username);
+console.log(extension);
